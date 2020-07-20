e63fa1d09e7433b011a7c584b1e498fe183ee995
LudwigBoess <lboess@usm.lmu.de>
2020-07-20 11:00:59 +0200

    Finished cutting gas particles from halo

--- src/AddHotHaloToGalaxy.jl
+++ src/AddHotHaloToGalaxy.jl
@@ -8,6 +8,7 @@ module AddHotHaloToGalaxy
     function add_hot_halo_to_galaxy(parameterfile::String; verbose::Bool=true)
 
         if verbose
+            t1_total = time_ns()
             @info "Reading parameter file"
             t1 = time_ns()
         end
@@ -26,12 +27,67 @@ module AddHotHaloToGalaxy
             t1 = time_ns()
         end
 
-        pos_halo, vel_halo, u_halo, m_halo = construct_hot_halo(par)
+        pos_halo, vel_halo, u_halo, m_halo, rho_halo = construct_hot_halo(par)
 
         if par["verbose"]
             t2 = time_ns()
             @info "Done!"
             @info "Took $(output_time(t1,t2)) s"
+        end
+
+        if par["verbose"]
+            @info "Selecting IDs of Gas particles to be cut out"
+            t1 = time_ns()
+        end
+
+        cut_ids = cut_ids_from_halo(pos_halo, rho_halo, par)
+
+        if par["verbose"]
+            t2 = time_ns()
+            @info "Done!"
+            @info "Took $(output_time(t1,t2)) s"
+        end
+
+        npart_before = length(u_halo)
+
+        # only keep relevant particles
+        pos_halo = pos_halo[cut_ids,:]
+        vel_halo = vel_halo[cut_ids,:]
+        rho_halo = rho_halo[cut_ids]
+        u_halo   = u_halo[cut_ids]
+        m_halo   = m_halo[cut_ids]
+
+        npart_after = length(u_halo)
+        n_cut = length(findall(cut_ids .== false))
+
+        if ( npart_before - npart_after ) != n_cut
+            error("Wrrong number of particles cut!
+                   nbefore = $npart_before
+                   nafter  = $npart_after
+                   n_cut   = $n_cut
+                   npart_before - npart_after = $(npart_before - npart_after)")
+        end 
+
+        
+
+        if par["verbose"]
+            @info "Writing to file"
+            t1 = time_ns()
+        end
+
+        write_to_file(pos_halo, vel_halo, rho_halo, u_halo, m_halo, par)
+
+        if par["verbose"]
+            t2 = time_ns()
+            @info "Done!"
+            @info "Took $(output_time(t1,t2)) s"
+        end
+
+        # Output total time
+        if verbose
+            t2_total = time_ns()
+            println()
+            @info "Total runtime $(output_time(t1_total,t2_total)) s"
         end
 
     end
---
+++ src/cut_ids_from_disk.jl
@@ -0,0 +1,168 @@
+using TriangularShapedCloudInterpolation
+using ProgressMeter
+using Base.Threads 
+
+function calculate_COM(x)
+    com = zeros(3)
+    for i = 1:3
+        com[i] = sum(x[:,i]) / length(x[:,i])
+    end
+    for i = 1:3
+        x[:,i] .-= com[i]
+    end
+    return x
+end
+
+function run_tsc(pos_disk, rho, resx, resy, resz, verbose)
+
+    pos_disk_tsc = zeros(length(rho),3)
+
+    minx = minimum(pos_disk[:,1])
+    maxx = maximum(pos_disk[:,1]) .* (1.0+1.e-6)
+    dx   = -(minx - maxx) / resx
+    pos_disk_tsc[:,1] = ( pos_disk[:,1] .- minx ) ./ dx 
+
+    miny = minimum(pos_disk[:,2])
+    maxy = maximum(pos_disk[:,2]) .* (1.0+1.e-6)
+    dy   = -(miny - maxy) / resy
+    pos_disk_tsc[:,2] = ( pos_disk[:,2] .- miny ) ./ dy 
+
+    minz = minimum(pos_disk[:,3])
+    maxz = maximum(pos_disk[:,3]) .* (1.0+1.e-6)
+    dz   = -(minz - maxz) / resz
+    pos_disk_tsc[:,3] = ( pos_disk[:,3] .- minz ) ./ dz
+
+    if verbose
+        @info "    Running TSC"
+        t1 = time_ns()
+    end
+
+    tsc = TSCInterpolation(rho, pos_disk_tsc[:,1], resx, 
+                                pos_disk_tsc[:,2], resy, 
+                                pos_disk_tsc[:,3], resz, 
+                            average=true)
+
+    if verbose
+        t2 = time_ns()
+        @info "    Done!"
+        @info "    Took $(output_time(t1,t2)) s"
+    end
+
+    return tsc, minx, miny, minz, dx, dy, dz
+
+end
+
+
+@inline function find_ids_from_density_criterion(   keep_id, pos_halo, rho_halo, rho_tsc, 
+                                                    resx, resy, resz,
+                                                    dx, dy, dz,
+                                                    minx, miny, minz
+                                                )
+
+    @showprogress "Filtering IDs..." for ix = 1:resx, iy = 1:resy, iz = 1:resz
+
+        lx = (ix-1)*dx + minx
+        rx = lx + dx
+        ly = (iy-1)*dy + miny
+        ry = ly + dy
+        lz = (iz-1)*dz + minz
+        rz = lz + dz
+
+        # @inbounds id = findall( (lx .< pos_halo[:,1] .< rx) .&
+        #                         (ly .< pos_halo[:,2] .< ry) .&
+        #                         (lz .< pos_halo[:,3] .< rz) )
+
+        
+        # idid = findall( rho_halo[id] .< 0.1*rho_tsc[ix, iy, iz] )
+
+        # keep_id[idid] .= false
+
+        #@threads 
+        # for i = 1:length(id)
+        #     if rho_halo[id[i]] < 0.1*rho_tsc[ix, iy, iz]
+        #         @inbounds keep_id[id[i]] = false
+        #     end
+        # end
+
+        @threads for i = 1:length(rho_halo)
+            @inbounds if (  (lx < pos_halo[i,2] < rx) &
+                            (ly < pos_halo[i,1] < ry) &
+                            (lz < pos_halo[i,3] < rz) )
+
+                if rho_halo[i] < 0.1*rho_tsc[ix, iy, iz]
+                    keep_id[i] = false
+                end
+            end
+        end
+    end
+
+    return keep_id
+
+end
+
+function cut_ids_from_halo(pos_halo, rho_halo, par)
+
+    if par["verbose"]
+        @info "  Reading galaxy data"
+        t1 = time_ns()
+    end
+
+    # read in data from galaxy
+    pos_disk, rho_disk, hsml_disk = read_pos_rho_hsml_galaxy(par["input_snap"], par["ic_format"])
+
+    if par["verbose"]
+        t2 = time_ns()
+        @info "  Done!"
+        @info "  Took $(output_time(t1,t2)) s"
+    end
+
+    if par["verbose"]
+        @info "  Shifting to center of mass."
+        t1 = time_ns()
+    end
+    # move to center of mass
+    x_halo = calculate_COM(pos_halo)
+    x_disk = calculate_COM(pos_halo)
+
+    if par["verbose"]
+        t2 = time_ns()
+        @info "  Done!"
+        @info "  Took $(output_time(t1,t2)) s"
+    end
+
+    resx = 50
+    resy = 50
+    resz = 50
+
+    if par["verbose"]
+        @info "  TSC Interpolation"
+        t1 = time_ns()
+    end
+
+    rho_tsc, minx, miny, minz, dx, dy, dz = run_tsc(pos_disk, rho_disk, resx, resy, resz, par["verbose"])
+
+    if par["verbose"]
+        t2 = time_ns()
+        @info "  Done!"
+        @info "  Took $(output_time(t1,t2)) s"
+    end    
+
+    if par["verbose"]
+        @info "  Filtering IDs by density criterion"
+        t1 = time_ns()
+    end
+
+    keep_id = trues(length(rho_halo))
+    keep_id = find_ids_from_density_criterion(  keep_id, pos_halo, rho_halo, rho_tsc, 
+                                                resx, resy, resz,
+                                                dx, dy, dz,
+                                                minx, miny, minz )
+
+    if par["verbose"]
+        t2 = time_ns()
+        @info "  Done! We need to delete $(length(findall(keep_id .== false) )) particles."
+        @info "  Took $(output_time(t1,t2)) s."
+    end
+
+    return keep_id
+end
